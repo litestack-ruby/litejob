7c0eb6147664a2708ff6c6de14998ba13b4659f2
Stephen Margheim <stephen.margheim@gmail.com>
2023-08-11 23:07:19 +0200

    Add SimpleCov for tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+end
+
 require "litejob"
 
 require "minitest/autorun"
