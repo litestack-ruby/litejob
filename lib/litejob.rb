0b1a64e4cb5c14b1232dc70990ed7a2b6604eae7
Stephen Margheim <stephen.margheim@gmail.com>
2023-08-14 11:30:30 +0200

    Move the Litejob interface code into a Concern module

--- lib/litejob.rb
+++ lib/litejob.rb
@@ -1,53 +1,13 @@
 # frozen_string_literal: true
 
 require_relative "litejob/version"
-require_relative "litejob/client"
+require_relative "litejob/concern"
 require_relative "litejob/server"
 
 # Litejob is responsible for providing an interface to job classes
 module Litejob
   def self.included(klass)
-    klass.extend(ClassMethods)
+    klass.extend(Concern)
   end
-
-  module ClassMethods
-    def perform_async(*params)
-      @litejob_options ||= {}
-      client.push(name, params, @litejob_options.merge(delay: 0, queue: queue_name))
-    end
-
-    def perform_at(time, *params)
-      @litejob_options ||= {}
-      delay = time.to_i - Time.now.to_i
-      client.push(name, params, @litejob_options.merge(delay: delay, queue: queue_name))
-    end
-
-    def perform_in(delay, *params)
-      @litejob_options ||= {}
-      client.push(name, params, @litejob_options.merge(delay: delay, queue: queue_name))
-    end
-    alias_method :perform_after, :perform_in
-
-    def delete(id)
-      client.delete(id)
-    end
-
-    def queue_as(queue_name)
-      @queue_name = queue_name.to_s
-    end
-
-    def litejob_options(options)
-      @litejob_options = options
-    end
-
-    private
-
-    def queue_name
-      @queue_name || "default"
-    end
-
-    def client
-      @client ||= Client.new
-    end
   end
 end
---
+++ lib/litejob/concern.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative "client"
+
+module Litejob
+  # Litejob::Concern is responsible for providing an interface to job classes
+  module Concern
+    def perform_async(*params)
+      @litejob_options ||= {}
+      client.push(name, params, @litejob_options.merge(delay: 0, queue: queue_name))
+    end
+
+    def perform_at(time, *params)
+      @litejob_options ||= {}
+      delay = time.to_i - Time.now.to_i
+      client.push(name, params, @litejob_options.merge(delay: delay, queue: queue_name))
+    end
+
+    def perform_in(delay, *params)
+      @litejob_options ||= {}
+      client.push(name, params, @litejob_options.merge(delay: delay, queue: queue_name))
+    end
+    alias_method :perform_after, :perform_in
+
+    def delete(id)
+      client.delete(id)
+    end
+
+    def queue_as(queue_name)
+      @queue_name = queue_name.to_s
+    end
+
+    def litejob_options(options)
+      @litejob_options = options
+    end
+
+    private
+
+    def queue_name
+      @queue_name || "default"
+    end
+
+    def client
+      @client ||= Client.new
+    end
+  end
+end
