7b44ff07d8186dae9519e38b653fd3ea249cddc0
Stephen Margheim <stephen.margheim@gmail.com>
2023-08-14 11:30:55 +0200

    Add Litejob configuration
    For now, only can configure a logger

--- lib/litejob.rb
+++ lib/litejob.rb
@@ -3,11 +3,26 @@
 require_relative "litejob/version"
 require_relative "litejob/concern"
 require_relative "litejob/server"
+require "logger"
 
-# Litejob is responsible for providing an interface to job classes
 module Litejob
   def self.included(klass)
     klass.extend(Concern)
   end
+  
+  Configuration = Struct.new(:logger)
+  
+  def self.configuration
+    @configuration ||= Configuration.new(
+      _logger = Logger.new($stdout),
+    )
+  end
+  
+  def self.configure
+    yield(configuration)
+  end
+  
+  def self.logger
+    configuration.logger
   end
 end
